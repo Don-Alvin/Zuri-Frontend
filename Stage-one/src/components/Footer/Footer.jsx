7e253cca17cc574a7172ee021bea00a08b87b8e1
Don-Alvin <alvindon41@gmail.com>
2022-11-04 17:47:40 +0300

    Final design and responsiveness

--- Stage-one/src/components/Footer/Footer.jsx
+++ Stage-one/src/components/Footer/Footer.jsx
@@ -3,16 +3,13 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <>
       <footer className='footer'>
-        <div className='footer__line'></div>
         <div className="footer__content">
           <div className="footer__img-1"></div>
           <span className="footer__text">HNG Internship 9 Frontend Task</span>
           <div className='footer__img-2'></div>
         </div>
       </footer>
-    </>
   )
 }
 
