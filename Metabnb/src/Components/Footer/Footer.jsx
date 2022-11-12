a3394b77b28cb0c4f0ac69195a115588babcae65
Don-Alvin <alvindon41@gmail.com>
2022-11-12 14:52:44 +0300

    Footer styled

--- Metabnb/src/Components/Footer/Footer.jsx
+++ Metabnb/src/Components/Footer/Footer.jsx
@@ -8,9 +8,11 @@ const Footer = () => {
 
   return (
     <footer className={classes.footer}>
+
       <div className={classes.footer__content}>
+
         <div className={classes.footer__logo_socials}>
-          <img src="" alt="footer__logo" className={classes.footer__content_logo} />
+          <img src="../../src/assets/images/footerlogo.png" alt="footer__logo" className={classes.footer__content_logo} />
           <div className={classes.footer__content_socials}>
             <div className={classes.socials}>
               <FaFacebookF />
@@ -18,44 +20,44 @@ const Footer = () => {
               <FaTwitter />
             </div>
             <div className={classes.copyright}>
-            <span>&#169; {currentYear} Metabnb</span>
+              <span>&#169; {currentYear} Metabnb</span>
             </div>
           </div>
         </div>
 
-        <div className={classes.footer__content_community}>
-          <h1 className={classes.heading} >Community</h1>
-          <div className={classes.community_content}>
-            <ul>
-              <li>NFT</li>
-              <li>Tokens</li>
-              <li>Landlords</li>
-              <li>Discord</li>
-            </ul>
+        <div className={classes.footer__content_list}>
+          <div className={classes.footer__content_item}>
+            <h1 className={classes.heading} >Community</h1>
+            <div className={classes.community_content}>
+              <ul>
+                <li>NFT</li>
+                <li>Tokens</li>
+                <li>Landlords</li>
+                <li>Discord</li>
+              </ul>
+            </div>
           </div>
-        </div>
-
-        <div className={classes.footer__content_places}>
-          <h1 className={classes.heading} >Places</h1>
-          <div className={classes.places_content}>
-            <ul>
-              <li>Castle</li>
-              <li>Farms</li>
-              <li>Beach</li>
-              <li>Learn more</li>
-            </ul>
+          <div className={classes.footer__content_item}>
+            <h1 className={classes.heading} >Places</h1>
+            <div className={classes.places_content}>
+              <ul>
+                <li>Castle</li>
+                <li>Farms</li>
+                <li>Beach</li>
+                <li>Learn more</li>
+              </ul>
+            </div>
           </div>
-        </div>
-
-        <div className={classes.footer__content_aboutus}>
-          <h1 className={classes.heading} >About us</h1>
-          <div className={classes.aboutus_content}>
-            <ul>
-              <li>Roadmap</li>
-              <li>Creators</li>
-              <li>Career</li>
-              <li>Contact us</li>
-            </ul>
+          <div className={classes.footer__content_item}>
+            <h1 className={classes.heading} >About us</h1>
+            <div className={classes.aboutus_content}>
+              <ul>
+                <li>Roadmap</li>
+                <li>Creators</li>
+                <li>Career</li>
+                <li>Contact us</li>
+              </ul>
+            </div>
           </div>
         </div>
 
