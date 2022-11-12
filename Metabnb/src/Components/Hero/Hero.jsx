cffb7630cd4c58389e6bda5aaf0f4894c3774502
Don-Alvin <alvindon41@gmail.com>
2022-11-12 15:43:19 +0300

    Hero section styled

--- Metabnb/src/Components/Hero/Hero.jsx
+++ Metabnb/src/Components/Hero/Hero.jsx
@@ -4,18 +4,20 @@ import classes from './Hero.module.css'
 const Hero = () => {
   return (
     <section className={classes.hero}>
-      <div className="hero_text">
+      <div className={classes.hero_text}>
         <h1 className={classes.hero_heading}>
           Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span>
         </h1>
         <p className={classes.hero_paragraph}>
           We provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone.
         </p>
+        <form className={classes.search}>
+          <input type="search" placeholder='Search for location' />
+          <div className={classes.btn_search}> 
+            <span>Search</span>
+          </div>
+        </form>
       </div>
-      <form className={classes.search}>
-        <input type="search" placeholder='Search for location' />
-        <span className='{classes.btn_search}'> Search</span>
-      </form>
       <div className="hero_image">
         <img src="../../src/assets/images/hero.png" alt="hero-image" />
       </div>
