1a5675c47f7344e6e7821ce9008ed385299eb02f
Don-Alvin <alvindon41@gmail.com>
2022-11-13 15:09:46 +0300

    Patners section finished

--- Metabnb/src/Components/Partners/Partners.jsx
+++ Metabnb/src/Components/Partners/Partners.jsx
@@ -3,7 +3,11 @@ import classes from './Partners.module.css'
 
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <div className={classes.partners}>
+      <img src="../../src/assets/images/mbtoken.svg" alt="mbtoken" />
+      <img src="../../src/assets/images/metamask.svg" alt="metamask" />
+      <img src="../../src/assets/images/opensea.svg" alt="opensea" />
+    </div>
   )
 }
 
