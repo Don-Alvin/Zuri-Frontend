d9dd23d40643e3364ef643d2ae78eda2dcd7cf3d
Don-Alvin <alvindon41@gmail.com>
2022-11-13 20:42:02 +0300

    Data.js for the places to stay added

---
+++ Metabnb/src/data.js
@@ -0,0 +1,73 @@
+export default [
+	{
+		img: "../../src/assets/images/stay1.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay2.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay3.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay4.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay5.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay6.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay7.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+
+	{
+		img: "../../src/assets/images/stay8.png",
+		title: "Desert king",
+		price: "1MTB per night",
+		distance: "2345km away",
+		availabity: "available for two weeks stay",
+		rating: "../../src/assets/images/rating.svg",
+	},
+];
