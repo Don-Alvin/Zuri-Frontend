ffe3cf6183780cac5151f1524c2ee8a963f05135
Don-Alvin <alvindon41@gmail.com>
2022-11-14 14:24:00 +0300

    NFT jsx done

--- Metabnb/src/Components/NFT/NFT.jsx
+++ Metabnb/src/Components/NFT/NFT.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import clases from './NFT.module.css'
+import classes from './NFT.module.css'
 
 const NFT = () => {
   return (
-    <div>NFT</div>
+    <section className={classes.nft}>
+      <div className={classes.nft__text}>
+        <h1 className={classes.nft__heading}>
+          Metabnb NFTs
+        </h1>
+
+        <p className={classes.nft__paragraph}>
+          Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
+        </p>
+
+        <a href="#" className={`${classes.btn} ${classes.btn__nft}`}>Learn more</a>
+      </div>
+    </section>
   )
 }
 
