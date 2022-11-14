7b48f7fde1c80030f9d9887a161c33b90e98d27c
Don-Alvin <alvindon41@gmail.com>
2022-11-14 18:47:52 +0300

    Places to stay page done

--- Metabnb/src/Components/PlaceToStayCard/PlaceToStayCard.jsx
+++ Metabnb/src/Components/PlaceToStayCard/PlaceToStayCard.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import classes from './PlaceToStayCard.module.css'
+import Card from '../UI/Card/Card'
 
-const PlaceToStayCard = () => {
+const PlaceToStayCard = (props) => {
   return (
-    <div>PlaceToStayCard</div>
+    <div className={classes.placetostay__images}>
+        <Card>
+            <div className={classes.content_wrapper}>
+                <img className={classes.image} src={props.img} alt="stay one" />
+                    
+                <div className={classes.placetostay__images_content}>
+                    <div className={classes.name__price}>
+                        <span className={classes.name}>{props.title}</span>
+                        <span className={classes.price}>{props.price}</span>
+                    </div>
+                    
+                    <div className={classes.distance__availability}>
+                        <span className={classes.distance}>{props.distance}</span>
+                        <span className={classes.availabity}>{props.availability}</span>
+                    </div>
+                    <div className={classes.rating}>
+                        <span><img src={props.rating} alt="rating" /></span>
+                    </div>
+                </div>
+            </div>
+        </Card>
+    </div>
   )
 }
 
--- Metabnb/src/Components/Stay/Stay.jsx
+++ Metabnb/src/Components/Stay/Stay.jsx
@@ -1,64 +1,68 @@
 import React from 'react'
 import { BsFilterSquare } from "react-icons/bs"
 import PlaceToStayCard from '../PlaceToStayCard/PlaceToStayCard'
-import StayCard from '../StayCards/StayCard'
 import classes from './Stay.module.css'
+import data from '../../data'
 
 const Stay = () => {
+
+  const cards = data.map(card => {
+    return (
+      <PlaceToStayCard
+        img={card.img}
+        title={card.title}
+        price={card.price}
+        distance={card.distance}
+        availability={card.availabity}
+        rating={card.rating}
+      />
+    )
+  })
+
   return (
     <main className={classes.main}>
-      <div className={classes.header}>
-
-        <div className={classes.categories}>
-
-          <ul className={classes.categories__list}>
-
-            <li className={classes.categories__item}>
-              <a href="">Restaurant</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Cottage</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Castle</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Fantasy City</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Beach</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Cabins</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Off-grid</a>
-            </li>
-
-            <li className={classes.categories__item}>
-              <a href="">Farm</a>
-            </li>
-
-          </ul>
-
-          <div className={classes.filter}>
-            <a href="">
-              <span>Locations</span>
-              <BsFilterSquare />
-            </a>
+      <div className={classes.stay__content}>
+        <div className={classes.header}>
+          <div className={classes.categories}>
+            <ul className={classes.categories__list}>
+              <li className={classes.categories__item}>
+                <a href="">Restaurant</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Cottage</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Castle</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Fantasy City</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Beach</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Cabins</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Off-grid</a>
+              </li>
+              <li className={classes.categories__item}>
+                <a href="">Farm</a>
+              </li>
+            </ul>
+            <div className={classes.filter}>
+              <a href="">
+                <span>Locations</span>
+                <div className={classes.filter_icon}>
+                  <BsFilterSquare />
+                </div>
+              </a>
+            </div>
           </div>
-
         </div>
-      </div>
-
-      <div className={classes.content}>
-        <PlaceToStayCard />
+        <div className={classes.content}>
+          {cards}
+        </div>
       </div>
     </main>
   )
