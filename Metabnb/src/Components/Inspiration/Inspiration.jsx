ee2472140dff2cc164aefc61ee22cd28158a5a57
Don-Alvin <alvindon41@gmail.com>
2022-11-14 12:59:37 +0300

    image styled

--- Metabnb/src/Components/Inspiration/Inspiration.jsx
+++ Metabnb/src/Components/Inspiration/Inspiration.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import classes from './Inspiration.module.css'
+import data from '../../data'
+import Card from '../UI/Card/Card'
+
 
 const Inspiration = () => {
   return (
+
     <section className={classes.inspiration}>
         <div className={classes.inspiration__content}>
 
@@ -11,8 +15,16 @@ const Inspiration = () => {
           </h1>
 
           <div className={classes.inspiration__images}>
+            <Card>
+              <img className={classes.image} src={data[0].img} alt="stay one" />
+              <div className={classes.inspiration__images_content}>
+                <div className={classes.name__price}>
+                  <span className={classes.name}>{data[0].title}</span>
+                </div> 
+              </div>
+            </Card>
           </div>
-            
+
         </div>
     </section>
   )
